d28c66ade68b2678a15dc87c742848b3356c4bb7
thejonfedor <74686265+thejonfedor@users.noreply.github.com>
2022-04-08 14:32:49 -0400

    new exercise code file

---
+++ ex30.py
@@ -0,0 +1,35 @@
+people = 30
+cars = 40
+trucks = 40
+
+
+if cars > people:
+    print("We should take the cars.")
+elif cars < people:
+    print("We should not take the cars.")
+else:
+    print("We can't decide.")
+
+if trucks > cars or cars == trucks:
+    print("That's too many trucks.")
+elif trucks < cars:
+    print("Maybe we could take the trucks.")
+else:
+    print("We still can't decide.")
+
+if people > trucks:
+    print("Alright, let's just take the trucks.")
+else:
+    print("Fine, let's stay home then.")
+
+'''
+Exercise Notes for ex30: Else and If
+1. Really complex if statements are generally bad style
+   Keep them tight and crisp
+2. elif and else are "else if" and "else" statments
+   that provide alternative statements to the first
+   boolean if-statement
+3. 
+   
+
+'''
