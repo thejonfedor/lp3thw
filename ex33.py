9eeb9d6f517e79bb8ec8cdc59bb65351aa908c36
thejonfedor <74686265+thejonfedor@users.noreply.github.com>
2022-04-11 14:57:02 -0400

    commit for exercise 33 while loops

---
+++ ex33.py
@@ -0,0 +1,22 @@
+i = 0
+numbers = []
+
+while i < 6:
+    print(f"At the top i is {i}")
+    numbers.append(i)
+
+    i = i + 1
+    print("Numbers now: ", numbers)
+    print(f"At the bottom i is {i}")
+
+
+print("The numbers: ")
+
+for num in numbers:
+    print(num)
+
+'''
+Exercise Notes for ex33: While Loops
+1. REAL flow controls. lezzzGOOO
+2. 
+'''
