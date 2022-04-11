472e85d5321348b3d377f27d27f7c7e16786da94
thejonfedor <74686265+thejonfedor@users.noreply.github.com>
2022-04-11 17:44:46 -0400

    commit changes for ex33 - added comments, notes, and a helpful print inside while loop

--- ex33.py
+++ ex33.py
@@ -8,6 +8,7 @@ while i < 6:
     i = i + 1
     print("Numbers now: ", numbers)
     print(f"At the bottom i is {i}")
+    print("----------") # added for clarity when running
 
 
 print("The numbers: ")
@@ -18,5 +19,12 @@ for num in numbers:
 '''
 Exercise Notes for ex33: While Loops
 1. REAL flow controls. lezzzGOOO
-2. 
+2. While loops are more...risky than for-loops
+   e.g. it's easier to write yourself into an infinite loop
+   Use for-loops whenever possible unless a
+   while-loop is straight up better for the job.
+3. Might be good to come up with a generic test for
+   while-loops that I can use in every one I write
+   that will stop the loop at some point no matter what
+   in case I make a mistake. Is this possible?
 '''
