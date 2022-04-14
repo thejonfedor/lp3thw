36ca016078da1ac22ce1c69810ad5c8ee2dfdc2f
thejonfedor <74686265+thejonfedor@users.noreply.github.com>
2022-04-14 06:29:22 -0400

    started the game and had some fun story-telling with this exercise 😁

---
+++ ex36.py
@@ -0,0 +1,40 @@
+# The goal here is to write my own walk-through game
+# Zed says to "spend a week making it as interesting
+# as possible." Probably won't. But I'll make something'
+
+from sys import exit
+
+def dead(reason):
+    print(reason, "Oy vey.")
+    exit(0)
+
+def start():
+    print("You wake up in a warm, soft bed.")
+    print("There are two doors in the room and a window.")
+    print("Go through the left door, right door, or window.")
+
+    choice = input("> ")
+
+    if choice == "left door":
+        # link to different function
+        i = 1
+    elif choice == "right door":
+        # link to more different function
+        i = 2
+    elif choice == "window":
+        # link to still other different function
+        i = 3
+    else:
+        print("REALLY? ", choice, "? Okay, well...")
+        print("You lay back down in the bed.")
+        print("Shortly, a wizard enters the room and casts a spell on you.")
+        print("You fall asleep very deep and never wake again.")
+        dead("You just got hosed by a puny Wizard. Dead.")
+
+start()
+
+'''
+Exercise Notes for ex36: Designing and Debugging
+1. 
+
+'''
