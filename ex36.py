02a689795f232eb14f8175c6f753d8b9e4dd9655
thejonfedor <74686265+thejonfedor@users.noreply.github.com>
2022-04-15 13:20:25 -0400

    added a grass function and some time delays for cmd line playability

--- ex36.py
+++ ex36.py
@@ -2,28 +2,53 @@
 # Zed says to "spend a week making it as interesting
 # as possible." Probably won't. But I'll make something'
 
+from pickle import TRUE
 from sys import exit
-import random
+import random, time
 
 def dead(reason):
     print(reason, "Oy vey. \n")
     exit(0)
 
+def grass(landing):
+
+    while True:
+        
+        if landing == "elf":
+            print("\nThe elf is angry and injured.")
+            print("It points at you, snaps its fingers, and you land")
+            print("back in the room you started, asleep, in bed...")
+            time.sleep(2)
+            angryElf = TRUE
+            start()
+        else landing == "soft":
+            print("There's a shiny crytal laying in the grass.")
+
+
 def window():
-    fate = random.randint(0,2)
+    fate = random.randint(0,3)
 
     if fate == 0:
         print("\nThe window breaks as you open it.")
-        print("You fall to your death")
+        print("You fall to your death...")
         dead("\nBad luck.")
     elif fate == 1:
         print("\nYou open the window and climb down the ladder")
+        grass("soft")
     elif fate == 2:
-        print("Jump out of the window and land on an angry elf.")
+        print("\nJump out of the window and land on an angry elf.")
+        grass("elf")
+    elif fate == 3:
+        print("\nThe window breaks as you open it.")
+        print("You fall to your death...in your DREAMS")
+        print("\nWhew - Good luck!")
+        time.sleep(2)
+        start()
 
 def start():
-    print("You wake up in a warm, soft bed.")
+    print("\nYou wake up in a warm, soft bed.")
     print("There are two doors in the room and a window.")
+    time.sleep(1)
     print("Go through the left door, right door, or window.")
 
     choice = input("> ")
@@ -39,9 +64,11 @@ def start():
     else:
         print("\nREALLY? ", choice, "? Okay, well... \n")
         print("You lay back down in the bed.")
+        time.sleep(4) # suspense
         print("Shortly, a wizard enters the room and casts a spell on you.")
-        print("You fall asleep very deep and never wake again.")
-        dead("You just got hosed by a puny Wizard. Dead.")
+        print("You fall into a very deep sleep and never wake again.")
+        time.sleep(3) # more suspense
+        dead("\nYou just got hosed by a puny Wizard. Dead.")
 
 start()
 
