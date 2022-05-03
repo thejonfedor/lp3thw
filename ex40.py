0167b97ab581d0978862325d67e5d7b6805ceb1a
thejonfedor <74686265+thejonfedor@users.noreply.github.com>
2022-05-03 08:24:19 -0500

    exercise 40 object oriented programming. need to troubleshoot this exercise more to use classes more correctly

--- ex40.py
+++ ex40.py
@@ -4,6 +4,7 @@ class Song(object):
         self.lyrics = lyrics
     
     def sing_me_a_song(self):
+        print("-" * 20)
         for line in self.lyrics:
             print(line)
 
@@ -12,8 +13,23 @@ happy_bday = Song(["Happy Birthday to you",
                    "So I'll stop right there."])
 
 bulls_on_parade = Song(["They rally around tha family",
-                        "With pockets full of sehlls."])
+                        "With pockets full of shells."])
+
+mm = Song(["Well the FCC",
+           "Won't let me be",
+           "Or let me be me",
+           "So let me see",
+           "They try to shut me down on MTV",
+           "But it feels so empty",
+           "WITHOUT ME."])
+
+coldPlay = "I hear Jerusalem bells are ringing, Roman cavalry choirs are singing."
 
 happy_bday.sing_me_a_song()
 
 bulls_on_parade.sing_me_a_song()
+
+mm.sing_me_a_song()
+# this doesn't work but I'm unsure why
+# error says the 'str' object has no attribute, sing_me_a_song()
+coldPlay.sing_me_a_song()
