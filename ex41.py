1dc8ae8bc6be4864208a767169415eb75c4eb6a3
thejonfedor <74686265+thejonfedor@users.noreply.github.com>
2022-05-03 09:04:39 -0500

    starting syntax for ex41

---
+++ ex41.py
@@ -0,0 +1,29 @@
+import random
+from urllib.request import urlopen
+import sys
+
+WORD_URL = "http://learncodethehardway.org/words.txt"
+words = []
+
+PHRASES = {
+    "class %%%(%%%):":
+        "Make a class named %%% that is-a %%%.",
+    "class %%%(object):\n\tdef __init__(self, ***)" :
+        "class %%% has-a __init__ that takes self and *** params.",
+    "class %%%(object):\n\tdef ***(self, @@@)":
+        "class %%% has-a function *** that takes self and @@@ params.",
+    "*** = %%%()":
+        "Set *** to an instance of class %%%.",
+    "***.***(@@@)":
+        "From *** get the *** function, call it with params self, @@@.",
+    "***.*** = '***'",
+        "From *** get the *** attribute and set it to '***'."
+}
+
+# do they want to drill phrases first
+if len(sys.argv) == 2 and sys.argv[1] == "english":
+    PHRASE_FIRST = True
+else:
+    PHRASE_FIRST = False
+
+# load up words from the website
