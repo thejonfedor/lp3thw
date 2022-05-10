974210b2cb7959a43f606f59b736b5d1afaec569
thejonfedor <74686265+thejonfedor@users.noreply.github.com>
2022-05-09 20:40:31 -0400

    starting the writing for exercise 42 - this cooooould be a long one, folks

---
+++ ex42.py
@@ -0,0 +1,19 @@
+## Animal is-a object (yes, sort of confusing) look at the extra credit
+class Animal(object):
+    pass
+
+## ??
+class Dog(Animal):
+
+    def __init__(self, name):
+        ## ??
+        self.name = name
+
+## ??
+class Cat(Animal):
+
+    def __init__(self, name):
+        ## ??
+        self.name = name
+
+## ??
