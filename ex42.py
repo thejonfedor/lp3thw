5ae6c5994c99cf6569fe2cc296e2dc3e9db308d7
thejonfedor <74686265+thejonfedor@users.noreply.github.com>
2022-05-12 21:37:23 -0400

    Working through ex42 exercise and writing the correct comments

--- ex42.py
+++ ex42.py
@@ -20,7 +20,7 @@ class Cat(Animal):
 class Person(object):
 
     def __init__(self, name):
-        ## ??
+        ## person has-a name of some kind
         self.name = name
 
         # Person has-a pet of some kind
@@ -32,7 +32,7 @@ class Employee(Person):
     def __init__(self, name, salary):
         ## ?? hmm what is this strange magic?
         super(Employee, self).__init__(name)
-        ## ??
+        ## employee has-a salary
         self.salary = salary
 
 ## ??
@@ -51,26 +51,26 @@ class Halibut(Fish):
 ## rover is-a Dog
 rover = Dog("Rover")
 
-## ??
+## Satan is-a Satan
 satan = Cat("Satan")
 
-## ??
+## Mary is-a person
 mary = Person("Mary")
 
-## ??
+## mary has-a pet, satan
 mary.pet = satan
 
-## ??
+## frank is-a employee who makes $120k / yy
 frank = Employee("Frank", 120000)
 
-## ??
+## rover is-a pet of frank
 frank.pet = rover
 
-## ??
+## flipper is-a Fish
 flipper = Fish()
 
-## ??
+## Crouse is-a salmon
 crouse = Salmon()
 
-## ??
+## harry is-a halibut
 harry = Halibut()
