abd15f775dc3f914b037690815dd889194053b45
thejonfedor <74686265+thejonfedor@users.noreply.github.com>
2023-06-19 11:05:30 -0400

    Added code to randomize the min and max for range in which to find primes

--- primeNums.py
+++ primeNums.py
@@ -1,5 +1,9 @@
 
-nums = range(1,30) # print(list(nums))
+import random
+
+min = random.randint(1,50) #set random minimum value for range
+max = random.randint(51,1000) #set random maximum value for range
+nums = range(min,max) #range of numbers in which we find all primes
 
 def is_prime(num): # function to check if number is prime or not
     for x in range(2,num):
@@ -7,10 +11,13 @@ def is_prime(num): # function to check if number is prime or not
             return False
     return True
 
-primes = list(filter(is_prime, nums))
+primes = list(filter(is_prime, nums)) #get a filtered list containing all primes in nums
+countPrimes = len(primes) #how many primes are in the list?
 
-print("#############")
-print(" ")
-print(f"These are all the prime numbers between", nums, ":", primes)
-print(" ")
-print("#############")
+
+print("\n#############\n")
+
+print(f"There are", countPrimes, "prime numbers", "between", min, "and", max, "\n")
+print(f"Here's a list of all the prime numbers in the above range:\n", primes)
+
+print("\n#############\n")
