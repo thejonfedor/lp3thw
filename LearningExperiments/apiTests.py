075adb2611c77ce08599c99baf81eea778bd02de
thejonfedor <74686265+thejonfedor@users.noreply.github.com>
2023-07-27 18:33:05 -0400

    file clean up and added api practice file

---
+++ LearningExperiments/apiTests.py
@@ -0,0 +1,18 @@
+import requests
+import json
+
+response = requests.get("https://randomuser.me/api/")
+# print(response.text)
+
+responseDetail = response.text
+
+parsed = json.loads(responseDetail)
+
+# How to pretty print JSON in py
+# https://stackoverflow.com/questions/12943819/how-to-prettyprint-a-json-file
+jsonFormat = json.dumps(parsed, indent = 4)
+
+print(jsonFormat)
+
+with open('jsonResponse.txt', 'w') as output:
+    output.write(jsonFormat)
