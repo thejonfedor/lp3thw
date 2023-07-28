9f8e1651b781814444a21ccc7e5c946d4cd0e5ff
thejonfedor <74686265+thejonfedor@users.noreply.github.com>
2023-07-27 22:32:53 -0400

    trying to debug urllib3 warnings so I can just call these damn APIs

--- LearningExperiments/apiTests.py
+++ LearningExperiments/apiTests.py
@@ -1,18 +1,22 @@
 import requests
 import json
+import warnings
 
-response = requests.get("https://randomuser.me/api/")
+# This shit below is NOT working, NOT surpressing warnings from the HTTP req. ugh.
+warnings.filterwarnings("ignore", category = UserWarning)
+
+response = requests.get("https://randomuser.me/api")
 # print(response.text)
 
 responseDetail = response.text
 
-parsed = json.loads(responseDetail)
+# parsed = json.loads(responseDetail)
 
 # How to pretty print JSON in py
 # https://stackoverflow.com/questions/12943819/how-to-prettyprint-a-json-file
-jsonFormat = json.dumps(parsed, indent = 4)
+# jsonFormat = json.dumps(parsed, indent = 4)
 
-print(jsonFormat)
+print(responseDetail)
 
-with open('jsonResponse.json', 'w') as output:
-    output.write(jsonFormat)
+# with open('jsonResponse.json', 'w') as output:
+#     output.write(jsonFormat)
