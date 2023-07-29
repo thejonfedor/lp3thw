c5510ab09e95eca7e19e7852ba355ad84915de9b
thejonfedor <74686265+thejonfedor@users.noreply.github.com>
2023-07-29 13:40:12 -0400

    FINALLY got this script working on MacOS Needed to fix my ability to capture the warning about urllib3 from the requests module

--- LearningExperiments/apiTests.py
+++ LearningExperiments/apiTests.py
@@ -1,22 +1,26 @@
-import requests
-import json
+# IT WORKS!
+# CRITICAL learning here: There is urllib3 warning associated with
+# python's requests module. The system python used to run this py
+# script is using an older SSL version and throws a warning when run
+# I tried a bunch of ways to catch the warning so it doesn't eff up
+# json response. Turns out the warning is thrown as soon as you
+# import the requests module. Moved my warnings import and catch
+# statement to the top of the script and viola! Works great now!
+
 import warnings
+warnings.filterwarnings("ignore")
 
-# This shit below is NOT working, NOT surpressing warnings from the HTTP req. ugh.
-warnings.filterwarnings("ignore", category = UserWarning)
+import requests, json
 
-response = requests.get("https://randomuser.me/api")
-# print(response.text)
+response = requests.get("https://randomuser.me/api").text
 
-responseDetail = response.text
-
-# parsed = json.loads(responseDetail)
+parsed = json.loads(response)
 
 # How to pretty print JSON in py
 # https://stackoverflow.com/questions/12943819/how-to-prettyprint-a-json-file
-# jsonFormat = json.dumps(parsed, indent = 4)
+jsonFormat = json.dumps(parsed, indent = 4)
 
-print(responseDetail)
+print(jsonFormat)
 
 # with open('jsonResponse.json', 'w') as output:
 #     output.write(jsonFormat)
