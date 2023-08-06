e3e7426e768234c17ed4c67493fb3b3e0703ab9c
thejonfedor <74686265+thejonfedor@users.noreply.github.com>
2023-08-06 14:15:19 -0400

    I formatted my new json file incorrectly. Troubleshooting got me to the right one New users file reads great now

--- LearningExperiments/printDict.py
+++ LearningExperiments/printDict.py
@@ -5,6 +5,8 @@ with open('userBase.json', 'r') as output:
     users = json.loads(read)
     keys = users.keys()
 
-print(f"Users is object type: {type(users)}")
+print(f"\nUsers is object type: {type(users)}")
 
-print(f"Users dict has these keys: {keys}")
+print(f"\nUsers dict has these keys: {keys}")
+
+print(f"\nUsers looks like this: {users}")
